41682de807d5e404c6ae14a76008af5ce39382ff
tbsnfruits <llinoa22@gmail.com>
2022-07-09 22:11:34 +0900

    complete event listener

--- background.js
+++ background.js
@@ -6,16 +6,16 @@ function insertImage() {
         // var text = document.createTextNode("Tutorix is the best e-learning platform");
 
         var tag = document.createElement("img");
-        tag.src = chrome.runtime.getURL("/images/nongdam.jpg"); // works!
+        tag.src = chrome.runtime.getURL("/images/ogg_pressed.gif"); // works!
 
 
         tag.id = 'imageAdded';
-        tag.style.width = "50%";
+        tag.style.width = "80%";
         tag.style.height = "auto";
         // tag.style.backgroundColor = "green";
         tag.style.position = "fixed";
-        tag.style.top = "100px";
-        tag.style.left = "20%";
+        tag.style.top = "10%";
+        tag.style.left = "10%";
         // tag.style.backgroundImage =  chrome.runtime.getURL('/images/nongdam.jpg'); // does not work
 
       // 
@@ -26,13 +26,20 @@ function insertImage() {
     };
   }
   
-chrome.action.onClicked.addListener((tab) => {  // insert script in DOM when extension icon cliked
-    if(!tab.url.includes("chrome://")) {      
-      chrome.scripting.executeScript({          // insert function insertImage in current tab webpage
-        target: { tabId: tab.id },
-        function: insertImage
-      });
-    }
+  chrome.webNavigation.onCompleted.addListener((tab) => {  // insert script in DOM when page is loaded
+
+    console.log("welp" + tab.tabId);
+
+    if(tab.url.includes("https://namu.wiki")) {      
+      
+    // if(changeInfo.status === 'complete') {
+        chrome.scripting.executeScript({          // insert function insertImage in current tab webpage
+          target: { tabId: tab.tabId },
+          function: insertImage
+    
+        })
+      // }
+      ;}
   });
 
 
